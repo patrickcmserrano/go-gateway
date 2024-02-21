7c769083357817433df7711ea818fcb7e122eac4
Patrick Serrano <patrick@molequedeideias.net>
2024-02-21 18:53:50 -0300

    transaction_dto + usecase test ExecuteInvalidCreditCard + mockgen repositoryMock

---
+++ usecase/process_transaction/process_transaction_test.go
@@ -0,0 +1,37 @@
+package process_transaction
+
+import (
+	"testing"
+	"time"
+)
+
+func TestProcessTransaction_ExecuteInvalidCreditCard(t *testing.T) {
+	input := TransactionDtoInput{
+		ID:                        "1",
+		AccountID:                 "1",
+		CreditCardNumber:          "400000000000000000000000",
+		CreditCardName:            "Wesley Silva",
+		CreditCardExpirationMonth: 12,
+		CreditCardExpirationYear:  time.Now().Year(),
+		CreditCardCVV:             123,
+		Amount:                    200,
+	}
+	expectedOutput := TransactionDtoOutput{
+		ID:           "1",
+		Status:       entity.REJECTED,
+		ErrorMessage: "invalid credit card number",
+	}
+
+	ctrl := gomock.NewCrontroller(t)
+	defer ctrl.Finish()
+
+	repositoryMock := mock_repository.NewMockTransactionRepository(ctrl)
+	repositoryMock.EXPECT().
+		Insert(input.ID, input.AccountID, input.Amount, expectedOutput.Status, expectedOutput.ErrorMessage).
+		Return(nil)
+
+	usecase := NewProcessTransaction(repositoryMock)
+	output, err := usecase.Execute(input)
+	assert.Nil(t, err)
+	assert.Equal(t, expectedOutput, output)
+}
