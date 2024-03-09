042174d2d2e565b73c8cf7163c3171c395ad88f7
Patrick Serrano <patrick@molequedeideias.net>
2024-03-09 19:26:06 -0300

    process transaction test

--- usecase/process_transaction/process_transaction.go
+++ usecase/process_transaction/process_transaction.go
@@ -30,5 +30,7 @@ func (p *ProcessTransaction) Execute(input TransactionDtoInput) (TransactionDtoO
 			ErrorMessage: invalidCC.Error(),
 		}, nil
 	}
+	transaction.SetCreditCard(*cc)
+	invalidTransaction := transaction.IsValid()
 	return TransactionDtoOutput{}, nil
 }
--- usecase/process_transaction/process_transaction_test.go
+++ usecase/process_transaction/process_transaction_test.go
@@ -39,3 +39,34 @@ func TestProcessTransaction_ExecuteInvalidCreditCard(t *testing.T) {
 	assert.Nil(t, err)
 	assert.Equal(t, expectedOutput, output)
 }
+
+func TestProcessTransaction_ExecuteRejectedCreditCard(t *testing.T) {
+	input := TransactionDtoInput{
+		ID:                        "1",
+		AccountID:                 "1",
+		CreditCardNumber:          "4193523830170205",
+		CreditCardName:            "Wesley Silva",
+		CreditCardExpirationMonth: 12,
+		CreditCardExpirationYear:  time.Now().Year(),
+		CreditCardCVV:             123,
+		Amount:                    12002,
+	}
+	expectedOutput := TransactionDtoOutput{
+		ID:           "1",
+		Status:       entity.REJECTED,
+		ErrorMessage: "Credit Card does not have limit for this transaction",
+	}
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	repositoryMock := mock_repository.NewMockTransactionRepository(ctrl)
+	repositoryMock.EXPECT().
+		Insert(input.ID, input.AccountID, input.Amount, expectedOutput.Status, expectedOutput.ErrorMessage).
+		Return(nil)
+
+	usecase := NewProcessTransaction(repositoryMock)
+	output, err := usecase.Execute(input)
+	assert.Nil(t, err)
+	assert.Equal(t, expectedOutput, output)
+}
